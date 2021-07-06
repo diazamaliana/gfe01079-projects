a4b6f08b50a84bbf753719a9c281fdf4d480e812
Diaz Amaliana <38368069+diazamaliana@users.noreply.github.com>
2021-07-06 15:19:25 +0700

    update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-
+function addPlaylist(){
+    var x = document.forms["formList"]["title"].value
+    if (x == "" || x == null){
+        alert("Form must be filled out");
+    }else {
+        alert("The form was submitted");
+    }
+}
