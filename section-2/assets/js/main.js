8ae5f1acd0b39ac57eee7b5bac44f6c6d270d3d5
Diaz Amaliana <diazamaliana@gmail.com>
2021-07-08 16:18:08 +0700

    homework 2 done

--- section-2/assets/js/main.js
+++ section-2/assets/js/main.js
@@ -1,8 +1,27 @@
-function addPlaylist(){
-    var x = document.forms["formList"]["title"].value
+function createPlaylist(){
+    var x = document.forms["formList"]["title","description"].value
     if (x == "" || x == null){
-        alert("Form must be filled out");
+        return alert("Form must be filled out");
     }else {
-        alert("The form was submitted");
+        return alert("The form was submitted");
     }
 }
+
+const titleTxt = document.getElementById("song-title");
+const artistTxt = document.getElementById("song-artist");
+const albumTxt = document.getElementById("song-album");
+const image = document.querySelector("img");
+
+fetch(
+    "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const { name, album, artists, external_urls} = data;
+      titleTxt.innerHTML = `<a href="${external_urls.spotify}">${name}</a>`;
+      artistTxt.innerHTML = artists.map((artist) => `<a href="${external_urls.spotify}">${artist.name}</a>`);
+      albumTxt.innerHTML = `<a href="${album.external_urls.spotify}">${album.name}</a>`;
+      image.src = album.images[0].url;
+    })
+    .catch((error) => alert(error));
