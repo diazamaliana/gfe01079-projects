6b2d9715e176a011c92848d19707f9cd779f6ce1
Diaz Amaliana <38368069+diazamaliana@users.noreply.github.com>
2021-07-09 13:22:52 +0700

    homework 2 done

--- section-2/assets/js/main.js
+++ section-2/assets/js/main.js
@@ -12,16 +12,17 @@ const artistTxt = document.getElementById("song-artist");
 const albumTxt = document.getElementById("song-album");
 const image = document.querySelector("img");
 
+//call to get a track info
 fetch(
     "https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json"
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      console.log(data); // Try to call above endpoint with a GET method, and console.log() the response
       const { name, album, artists, external_urls} = data;
       titleTxt.innerHTML = `<a href="${external_urls.spotify}">${name}</a>`;
       artistTxt.innerHTML = artists.map((artist) => `<a href="${external_urls.spotify}">${artist.name}</a>`);
       albumTxt.innerHTML = `<a href="${album.external_urls.spotify}">${album.name}</a>`;
       image.src = album.images[0].url;
     })
-    .catch((error) => alert(error));
+    .catch((error) => alert(error)); //Do alert() when there is an error
